d1d44d458cea363b7fd9d148512f0784411ba61c
Informatica <informatica@alumno.local>
2025-05-19 17:06:42 -0600

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,31 @@ import Navbar from './components/navbars/navbar.jsx';
 import Footer from './components/footer/footer.jsx';
 import AppRoutes from "./routes.jsx";
 import './App.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'light',  // Puedes configurarlo a 'dark' si lo prefieres
+    useSystemColorMode: false,
+  },
+});
 const App = () => {
 
   return (
-    <BrowserRouter>
-      <>
-        <Navbar />
-        <div className="card">
-          <AppRoutes />
-        </div>
-        {/*<Footer />*/}
-      </>
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <>
+          <Navbar />
+          <div className="card">
+            <AppRoutes />
+          </div>
+          {/*<Footer />*/}
+        </>
+      </BrowserRouter>
+      
+    </ChakraProvider>
+
   );
 };
 
